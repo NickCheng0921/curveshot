9af805064ae3ee21a64ccea3fffefdf00089e4b1
Amani Toussaint <amanit@mit.edu>
2021-06-30 22:26:55 -0700

    Fixed camera rotation

--- 3dDemo/CameraPivot.gd
+++ 3dDemo/CameraPivot.gd
@@ -1,13 +1,12 @@
-extends Position3D
+extends Camera
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-export var SENSITIVITY = 0.2
-export var SMOOTHNESS = 10
+export var SENSITIVITY := 0.2
+export var SMOOTHNESS := 10.0
 
-onready var cam = $"y-gimbal/Camera"
 var camera_input : Vector2
 var rotation_velocity : Vector2
 
@@ -23,21 +22,20 @@ func _input(event):
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-var i = 1
+var i := 1
 func _process(delta):
 	if(i >= 60):
 		i = 0
 		print($"../Pivot".rotation_degrees.z)
 	else:
 		i += 1
-	var rotation_tuple = $'y-gimbal'.global_transform.basis.get_euler()
-	rotation_velocity = rotation_velocity.linear_interpolate(camera_input * SENSITIVITY, delta * SMOOTHNESS)
-	cam.rotate_y(-deg2rad(rotation_velocity.x))
-	cam.rotate_x(-deg2rad(rotation_velocity.y))
-	#sync y rotation to player model
-
-	#$"../Pivot".rotation_degrees.z = rotation_tuple[1]
 	
-	#cam.rotation_degrees.x = clamp(cam.rotation_degrees.x, -90, 90)
-	#cam.rotation_degrees.y = clamp(cam.rotation_degrees.y, -90, 90)
+	rotation_velocity = rotation_velocity.linear_interpolate(camera_input * SENSITIVITY, delta * SMOOTHNESS)
+	rotation_degrees.x += -deg2rad(rotation_velocity.y)
+	rotation_degrees.y += -deg2rad(rotation_velocity.x)
+	# TODO: sync y rotation to player model
+	
+	
+	# Clamp vertical rotation
+	rotation_degrees.x = clamp(rotation_degrees.x, -90, 90)
 	camera_input = Vector2.ZERO
