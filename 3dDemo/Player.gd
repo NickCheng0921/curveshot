1489125e66c13c44d1a14aab834c9d58e1ed37da
NickCheng0921 <nickshacheng.0921@gmail.com>
2021-07-01 18:00:19 -0700

    Make player move in looking direction WIP

--- 3dDemo/Player.gd
+++ 3dDemo/Player.gd
@@ -5,17 +5,32 @@ export var speed = 14
 # The downward acceleration when in the air, in meters per second squared.
 export var fall_acceleration = 75
 export var jump_impulse = 20
+export var SENSITIVITY := 2
+export var SMOOTHNESS := 10.0
+
+onready var camera = $Camera
+onready var Pivot = $Pivot
+var camera_input : Vector2
+var rotation_velocity : Vector2
+var y_rotation = 0
 
 var velocity = Vector3.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		#camera input is a tuple of (x, y) movement
+		camera_input = event.relative
 
 #called every frame
+var i := 1
 func _physics_process(delta):
 	# We create a local variable to store the input direction.
 	var direction = Vector3.ZERO
+	var global_y_degree = deg2rad(global_transform.basis.get_euler()[1])
 	
 	# We check for each move input and update the direction accordingly.
 	if Input.is_action_pressed("move_right"):
@@ -33,6 +48,16 @@ func _physics_process(delta):
 		direction = direction.normalized()
 		#$Pivot.look_at(translation + direction, Vector3.UP) 
 
+	#mouse based rotation
+	rotation_velocity = rotation_velocity.linear_interpolate(camera_input * SENSITIVITY, delta * SMOOTHNESS)
+	camera.rotation_degrees.x += -deg2rad(rotation_velocity.y)
+	y_rotation = -deg2rad(rotation_velocity.x)
+	rotation_degrees.y += y_rotation #move mesh
+	
+	# Clamp vertical rotation
+	rotation_degrees.x = clamp(rotation_degrees.x, -90, 90)
+	camera_input = Vector2.ZERO
+
 	# Ground velocity
 	velocity.x = direction.x * speed
 	velocity.z = direction.z * speed
@@ -41,4 +66,10 @@ func _physics_process(delta):
 	# Moving the character
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y += jump_impulse
+	#print("velocity: ", velocity)
+	print(velocity.x, " ", velocity.z)
+	velocity.x *= -cos(global_y_degree)
+	velocity.z *= sin(global_y_degree)
+	print("    ", velocity.x, " ", velocity.z)
 	velocity = move_and_slide(velocity, Vector3.UP)
+	
